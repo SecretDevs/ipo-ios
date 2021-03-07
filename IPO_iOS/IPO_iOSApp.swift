3f40d5cf2137aec4e6c70275e9bcbb99ab7defd8
BigBey <bey.sviatoslav@yandex.ru>
2021-03-07 03:42:56 -0800

    Adding SwiftUI App.

---
+++ IPO_iOS/IPO_iOSApp.swift
@@ -0,0 +1,21 @@
+//
+//  IPO_iOSApp.swift
+//  IPO_iOS
+//
+//  Created by Ivanov Ivan on 03.03.2021.
+//
+//
+
+import SwiftUI
+
+@main
+struct IPO_iOSApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
