2c44f7eba6bbd6be166f038959d8d25ffee24967
jelliedfish <hungrydog757@gmail.com>
2021-03-28 02:13:41 +0300

    ToggleButtons screens logic has been done

--- IPO_iOS/Transactions/ToggleButtons/ToggleButton.swift
+++ IPO_iOS/Transactions/ToggleButtons/ToggleButton.swift
@@ -129,7 +129,6 @@ struct ToggleButtonsView: View {
                     }
                 }
 
-
             }
                     .padding(.vertical, 10)
                     .padding(.horizontal, 10)
