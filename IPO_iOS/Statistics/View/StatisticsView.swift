ca1821afdd3a775827ecc99e235fb1cb2275929a
sviatoslav <bey.sviatoslav@yandex.ru>
2021-03-29 22:52:29 +0300

    Almost all statistics graph is done except toggle buttons.

--- IPO_iOS/Statistics/View/StatisticsView.swift
+++ IPO_iOS/Statistics/View/StatisticsView.swift
@@ -6,12 +6,22 @@ import Foundation
 import SwiftUI
 import SwiftUICharts
 
-struct StatisticsView : View {
+struct StatisticsView: View {
 
     var body: some View {
-        VStack{
-            LineChartView(data: [12, 5, 6, 9, 1, 15], title: "Рост портфеля", legend: "Full screen", form: ChartForm.large)
-
+        VStack {
+            LineView(data: [12, 5, 6, 9, 1, 20], title: "Рост портфеля", legend: "+74%",
+                    style: ChartStyle(
+                            backgroundColor: .white,
+                            accentColor: Color("Blue"),
+                            gradientColor: GradientColor(start: Color("Blue").opacity(0.00), end: Color("Blue").opacity(1.00)),
+                            backgroundGradientColor: GradientColor(start: Color("Blue").opacity(0.21), end: Color("Blue").opacity(0)),
+                            indicatorDotColor: Color("Blue"),
+                            textColor: Color("DarkGrey"),
+                            legendTextColor: Color("DarkGrey"),
+                            fontName: "EuclidSquare-Regular",
+                            dropShadowColor: Color("DarkGrey")))
+            LineChartView(data: [12, 5, 6, 9, 1, 15.50], title: "Рост портфеля", legend: "Full screen", form: ChartForm.medium)
         }
     }
 }
